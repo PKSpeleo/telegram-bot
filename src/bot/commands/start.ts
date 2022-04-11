bdd5a0e0a0a08b7647357d37c6f6ca618b73cf87
Pavel Katkov <pkspeleo@gmail.com>
2022-04-11 21:43:08 +0200

    chore: add info about known user to start report

--- src/bot/commands/start.ts
+++ src/bot/commands/start.ts
@@ -1,8 +1,12 @@
 import { BotContext, BotProperties } from '../shared/interfaces';
 import { sendMessagesToBosses } from '../shared/boss';
+import { extractRights } from '../shared/rights';
 
 export async function reactOnStartCommand(ctx: BotContext, botProperties: BotProperties) {
-  const messageForBosses = `👀 ${ctx.from.first_name} ${ctx.from.last_name} (@${ctx.from.username}) ${ctx.from.id} started chat with Me`;
+  const { isUserMemberOfSupportedChats } = await extractRights(ctx, botProperties);
+  const messageForBosses = `👀 ${ctx.from.first_name} ${ctx.from.last_name} (@${
+    ctx.from.username
+  }) ${isUserMemberOfSupportedChats ? '+' : '-'} ${ctx.from.id} started chat with Me`;
   await sendMessagesToBosses(botProperties.ADMIN_ID, ctx, messageForBosses);
   ctx.reply(`Hello, ${ctx.from.first_name} ${ctx.from.last_name} (@${ctx.from.username})!`);
 }

57a5b76109661f574cebe45e332608efa0368c13
Pavel Katkov <pkspeleo@gmail.com>
2022-04-17 23:28:52 +0200

    chore: add log request for any strings quantity

--- src/bot/commands/log.ts
+++ src/bot/commands/log.ts
@@ -7,10 +7,20 @@ export async function reactOnLogCommand(
   botProperties: BotProperties,
   logger: Logger
 ) {
+  const messageParts = ctx.message.text.split(' ');
+  //TODO be careful - bot will restart if to big message, need to handle it without restart
+  let stringsAmount = 10;
+  if (messageParts.length == 2) {
+    const amountString: string = messageParts[1];
+    const isNum = /^\d+$/.test(amountString);
+    const amountNumber = isNum ? Number(amountString) : NaN;
+    stringsAmount = Number.isInteger(amountNumber) ? amountNumber : stringsAmount;
+  }
+
   const { isAdmin } = await extractRights(ctx, botProperties);
   const isPrivateChat = ctx.from.id == ctx.chat.id;
   if (isAdmin && isPrivateChat) {
-    const lastLogLines = await logger.getLastLogStrings();
+    const lastLogLines = await logger.getLastLogStrings(stringsAmount);
     ctx.reply(
       `Total strings in the Log: ${lastLogLines.totalStringsAmount}
 last ${lastLogLines.requestedStringsAmount} strings of Log: \n${lastLogLines.logStrings.join(

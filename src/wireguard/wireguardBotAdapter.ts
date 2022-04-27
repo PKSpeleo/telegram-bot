dab28ee33329b0c623db2867abbd050d751c27b0
Pavel Katkov <pkspeleo@gmail.com>
2022-04-28 00:09:54 +0200

    chore: comment

--- src/wireguard/wireguardBotAdapter.ts
+++ src/wireguard/wireguardBotAdapter.ts
@@ -1,6 +1,9 @@
 import { PromiseQueue } from '../utils/promiseQueue';
 import { GetConfigFile, getConfigFile } from './wireguard';
 
+//TODO set maximum keys limit when generating it
+const MAX_KEYS_PER_USER = 3;
+
 export class WireguardBotAdapter {
   private queue = new PromiseQueue();
 

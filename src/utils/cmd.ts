c70d268dd33f07f123bf7b2ac6dc487a3e3074f8
Pavel Katkov <pkspeleo@gmail.com>
2022-04-30 00:54:43 +0200

    fix(wg): fix conf sync 5

--- src/utils/cmd.ts
+++ src/utils/cmd.ts
@@ -2,11 +2,17 @@ import * as child_process from 'child_process';
 
 export async function execChildProcess(cmd: string): Promise<string> {
   return new Promise((resolve, reject) => {
-    child_process.exec(cmd, (err, stdout) => {
-      if (err) {
-        reject(err);
+    child_process.exec(
+      cmd,
+      {
+        shell: 'bash'
+      },
+      (err, stdout) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(String(stdout).trim());
       }
-      resolve(String(stdout).trim());
-    });
+    );
   });
 }
